319ec90226ff77580f411565e098990bd1021da9
leo-marto <leomartorell@gmail.com>
2024-10-26 19:00:29 -0300

    infra

--- script.sh
+++ script.sh
@@ -67,11 +67,11 @@ terraform init
 if [ $? -eq 0 ]; then
   echo ""
   echo "Ejecutando Terraform Plan"
-  terraform plan -var-file="vars.tfvars"
+  terraform plan -var-file="vars.tfvars" -lock=false
   if [ $? -eq 0 ]; then
     echo ""
     echo "Ejecutando Terraform Apply"
-    terraform apply -var-file="vars.tfvars" -auto-approve
+    terraform apply -var-file="vars.tfvars" -auto-approve -lock=false
     else
     exit 1
   fi
